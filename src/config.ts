199558fb4c623dd31f14c575dbd6b7a4a8aa6ab8
Wei Sun <sagasw@gmail.com>
2023-07-21 05:17:12 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "https://astro-paper.pages.dev/",
   author: "SW",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "My personal blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -22,7 +22,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/dalian-js",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   }
