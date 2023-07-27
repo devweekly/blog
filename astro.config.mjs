efe13e7a66a8addff26b025e7ce1c96c95695033
Wei Sun <sagasw@gmail.com>
2023-07-27 08:15:25 +0000

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import embeds from "astro-embed/integration"
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +18,8 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    mdx(),
+    embeds(),
   ],
   markdown: {
     remarkPlugins: [
