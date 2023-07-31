7dff84b1fec479e966e694caf0648ddc3a342898
Wei Sun <sagasw@gmail.com>
2023-07-31 12:26:11 +0000

    add

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://techweekly.pages.dev/",
+  website: "https://devweekly.github.io/",
   author: "SW",
   desc: "Tech weekly newsletter",
   title: "TechWeekly",
